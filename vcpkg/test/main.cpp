9bb61ee74921f8e9f3949fd14d251c84e78ef29d
Ivan Gagis <igagis@gmail.com>
2024-11-25 22:24:52 +0200

    vcpkgize

---
+++ vcpkg/test/main.cpp
@@ -0,0 +1,15 @@
+#include <nitki/queue.hpp>
+
+int main(int argc, const char** argv){
+    nitki::queue queue;
+
+    queue.push_back([](){
+        std::cout << "Hello nitki!" << std::endl;
+    });
+
+    std::cout << "queue.size() = " << queue.size() << std::endl;
+
+    queue.pop_front()();
+
+    return 0;
+}
